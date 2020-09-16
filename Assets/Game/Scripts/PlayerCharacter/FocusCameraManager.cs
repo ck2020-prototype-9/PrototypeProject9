f5ac6eb6c1e4aee8d1e30eeba00d32cc1f551623
SteamB23 <steamb23@outlook.com>
2020-09-17 00:47:29 +0900

    카메라 작업

---
+++ Assets/Game/Scripts/PlayerCharacter/FocusCameraManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusCameraManager : MonoBehaviour
+{
+    [SerializeField] Transform targetTransform;
+    [SerializeField] Vector3 offset;
+    [Space]
+    [SerializeField] Transform rootTransform;
+    [SerializeField] float positionSmoothTime = 0.2f;
+
+    #region SmoothDamp
+
+    private Vector3 positionSmooth;
+
+    #endregion
+
+    void Awake()
+    {
+        rootTransform.position = targetTransform.position + offset;
+    }
+
+    private void Update()
+    {
+        rootTransform.position = Vector3.SmoothDamp(rootTransform.position, targetTransform.position + offset, ref positionSmooth, positionSmoothTime);
+    }
+}
