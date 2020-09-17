90a55a23599d981028fd70dab390cf9cf8f0f5ad
SteamB23 <steamb23@outlook.com>
2020-09-18 01:20:51 +0900

    게임 클리어 변수 추가

---
+++ Assets/Game/Scripts/GameClearTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameClearTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // 게임 클리어 처리
+
+            GameStageManager.Instance.IsGameClear = true;
+        }
+    }
+}
--- Assets/Game/Scripts/Manager/GameStageManager.cs
+++ Assets/Game/Scripts/Manager/GameStageManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class GameStageManager : MonoBehaviour, IStageResettable
 {
     [SerializeField] DirectorManager gameOverDirectorManager;
+    [SerializeField] DirectorManager gameClearDirecterManager;
     [SerializeField] PlayerCharacterManager playerCharacterManager;
     [SerializeField] FocusCameraManager focusCameraManager;
 
@@ -15,6 +16,8 @@ public class GameStageManager : MonoBehaviour, IStageResettable
     public FocusCameraManager FocusCameraManager => this.focusCameraManager;
 
     bool isGameOver = false;
+    bool isGameClear = false;
+
     public bool IsGameOver
     {
         get => isGameOver;
@@ -25,6 +28,24 @@ public class GameStageManager : MonoBehaviour, IStageResettable
                 GameOverDirectorManager.StartDirecting();
             }
             isGameOver = value;
+        }
+    }
+
+    public bool IsGameClear
+    {
+        get => isGameClear;
+        set
+        {
+            if (!isGameOver)
+            {
+                if (isGameClear==false && value == true)
+                {
+                    // TODO: 게임 클리어시 클리어 데이터 저장 구현 해야함
+
+                    // 게임 클리어 연출 시작
+                    gameClearDirecterManager.StartDirecting();
+                }
+            }
         }
     }
 
@@ -47,6 +68,7 @@ public class GameStageManager : MonoBehaviour, IStageResettable
     public void StageReset()
     {
         IsGameOver = false;
+        gameClearDirecterManager.StageReset();
         GameOverDirectorManager.StageReset();
         PlayerCharacterManager.StageReset();
     }
