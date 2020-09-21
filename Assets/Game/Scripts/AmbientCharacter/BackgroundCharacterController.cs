3bd2eed33cfed127b00bec60bb2d51ab6697ebc6
SteamB23 <steamb23@outlook.com>
2020-09-21 14:23:12 +0900

    배경 캐릭터 컨트롤러 구현

---
+++ Assets/Game/Scripts/AmbientCharacter/BackgroundCharacterController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+[Serializable]
+public class BackgroundCharacterControllerEvent : UnityEvent<BackgroundCharacterController> { }
+
+public class BackgroundCharacterController : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent navMeshAgent;
+    [Tooltip("")]
+    [SerializeField] float speed = 3.5f;
+    [SerializeField] Transform[] waypoints;
+    [SerializeField] bool isLoop;
+    [SerializeField] BackgroundCharacterControllerEvent arriveEvent = new BackgroundCharacterControllerEvent();
+    [SerializeField] bool isShowWaypointLine;
+
+    private int waypointsStep = 0;
+
+    public NavMeshAgent NavMeshAgent => navMeshAgent;
+    public bool IsLoop
+    {
+        get => isLoop;
+        set
+        {
+            isLoop = value;
+            if (waypointsStep >= waypoints.Length)
+            {
+                waypointsStep = 0;
+            }
+        }
+    }
+    public BackgroundCharacterControllerEvent ArriveEvent => arriveEvent;
+
+    public Transform[] GetWaypoints() => waypoints;
+    public void SetWaypoints(Transform[] waypoints)
+    {
+        waypointsStep = 0;
+        this.waypoints = waypoints;
+    }
+
+    private void Start()
+    {
+        GotoCurrentPoint();
+    }
+
+    private void Update()
+    {
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (isShowWaypointLine)
+        {
+            for (int i = 0; i < waypoints.Length; i++)
+            {
+                int j = (i + 1) % waypoints.Length;
+                
+                if (j != 0 || isLoop)
+                {
+                    Handles.color = Color.green;
+                    Handles.DrawLine(waypoints[i].position, waypoints[j].position);
+                    //Gizmos.color = Color.green;
+                    //Gizmos.DrawLine(waypoints[i].position, waypoints[j].position);
+                }
+            }
+        }
+    }
+
+    private void GotoNextPoint()
+    {
+        NextStep();
+        GotoCurrentPoint();
+    }
+
+    private void GotoCurrentPoint()
+    {
+        if (waypointsStep < waypoints.Length)
+        {
+            navMeshAgent.SetDestination(waypoints[waypointsStep].position);
+        }
+    }
+
+    private int NextStep()
+    {
+        if (IsLoop)
+        {
+            waypointsStep = (waypointsStep + 1) % waypoints.Length;
+            navMeshAgent.autoBraking = false;
+        }
+        else if (waypointsStep < waypoints.Length)
+        {
+            waypointsStep += 1;
+            navMeshAgent.autoBraking = false;
+        }
+        else
+        {
+            navMeshAgent.autoBraking = true;
+        }
+        return waypointsStep;
+    }
+}
