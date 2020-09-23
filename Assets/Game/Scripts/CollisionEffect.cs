611e74f8c11056d8f04bb0f78c1e30b8ca7e223b
SteamB23 <steamb23@outlook.com>
2020-09-24 03:52:32 +0900

    충돌 이펙트 스크립트 추가

---
+++ Assets/Game/Scripts/CollisionEffect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionEffect : MonoBehaviour
+{
+    [SerializeField] ParticleSystem effect;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip[] audioClips;
+    [SerializeField] LayerMask targetLayer = -1;
+    [SerializeField] Vector3 offset = new Vector3(0, 0.01f, 0);
+    [SerializeField] Vector3 scale = new Vector3(1, 1, 1);
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((collision.gameObject.layer & targetLayer) != 0)
+        {
+            foreach (var contact in collision.contacts)
+            {
+                var effectInstance = Instantiate(effect);
+                var effectTransform = effectInstance.transform;
+                effectTransform.position = contact.point + offset;
+                var effectScale = effectTransform.localScale;
+                effectTransform.localScale = new Vector3(effectScale.x * scale.x, effectScale.y * scale.y, effectScale.z * scale.z);
+                audioSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Length)]);
+                break;
+            }
+        }
+    }
+}
