e77d20246b24735556f7811e3ef78248db3c0b8b
SteamB23 <steamb23@outlook.com>
2020-09-24 11:27:02 +0900

    충격량 계산 최적화 및 세부 조정

--- Assets/Game/Scripts/CollisionEffect.cs
+++ Assets/Game/Scripts/CollisionEffect.cs
@@ -7,9 +7,11 @@ public class CollisionEffect : MonoBehaviour
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip[] audioClips;
     [SerializeField] LayerMask targetLayer = -1;
-    [SerializeField] Vector3 offset = new Vector3(0, 0.01f, 0);
+    [SerializeField] Vector3 offset = new Vector3(0, 0.03f, 0);
     [SerializeField] Vector3 scale = new Vector3(1, 1, 1);
 
+    [SerializeField] float scalePerVelocity = 0.5f;
+    [SerializeField] float maxScale = 2f;
     [SerializeField] float volumeScalePerVelocity = 0.5f;
     [SerializeField] float maxVolumeScale = 2f;
 
@@ -21,10 +23,19 @@ public class CollisionEffect : MonoBehaviour
             {
                 var effectInstance = Instantiate(effect);
                 var effectTransform = effectInstance.transform;
+
                 effectTransform.position = contact.point + offset;
+
+                var impulse = Vector3.Scale(collision.relativeVelocity, contact.normal).magnitude;
                 var effectScale = effectTransform.localScale;
-                effectTransform.localScale = new Vector3(effectScale.x * scale.x, effectScale.y * scale.y, effectScale.z * scale.z);
-                audioSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Length)], Mathf.Min(collision.relativeVelocity.magnitude * volumeScalePerVelocity, maxVolumeScale));
+
+                effectTransform.localScale = Vector3.Scale(effectScale, scale) * Mathf.Min(impulse * scalePerVelocity, maxScale);
+
+                audioSource.PlayOneShot(
+                    audioClips[Random.Range(0, audioClips.Length)],
+                    Mathf.Min(
+                        impulse * volumeScalePerVelocity,
+                        maxVolumeScale));
                 break;
             }
         }
