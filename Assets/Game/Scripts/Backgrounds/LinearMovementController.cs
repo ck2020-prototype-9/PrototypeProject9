30217708eaf2236a363f86c9b5de0aa1ca320f9a
SteamB23 <steamb23@outlook.com>
2020-10-08 01:26:30 +0900

    일직선 이동 컨트롤러 구현

---
+++ Assets/Game/Scripts/Backgrounds/LinearMovementController.cs
@@ -0,0 +1,51 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+class LinearMovementController : MonoBehaviour
+{
+    [SerializeField] Transform from;
+    [SerializeField] Transform to;
+    [SerializeField] bool isLoop;
+    [SerializeField] float speed;
+    [SerializeField] bool isShowWaypointLine;
+
+    Transform thisTransform;
+
+    Tween tween;
+
+    private void Start()
+    {
+        thisTransform = this.transform;
+
+        thisTransform.position = from.position;
+        thisTransform.LookAt(to);
+
+        // duration 계산
+        float duration = Vector3.Distance(from.position, to.position) / speed;
+
+        tween = thisTransform.DOMove(to.position, duration)
+            .SetEase(Ease.Linear);
+
+        if (isLoop)
+        {
+            tween.SetLoops(-1);
+        }
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (!Application.IsPlaying(this))
+            this.transform.position = from.position;
+        if (isShowWaypointLine)
+        {
+            Handles.color = Color.cyan;
+            Handles.DrawLine(from.position, to.position);
+        }
+    }
+#endif
+}
