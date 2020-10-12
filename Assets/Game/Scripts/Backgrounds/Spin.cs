fbbbd24b2275b6de0d35b530a6bd1d20a79aae0f
SteamB23 <steamb23@outlook.com>
2020-10-12 23:27:36 +0900

    메인 메뉴 화면 연출 및 라이트맵

---
+++ Assets/Game/Scripts/Backgrounds/Spin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 10;
+    [SerializeField] Vector3 axis = Vector3.forward;
+
+    float currentAngle;
+    void Update()
+    {
+        currentAngle += rotationSpeed * Time.deltaTime;
+        //transform.rotation = Quaternion.AngleAxis(currentAngle, axis);
+        transform.Rotate(axis * rotationSpeed * Time.deltaTime);
+    }
+}
